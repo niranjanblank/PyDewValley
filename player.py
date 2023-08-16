7125b206adcccbfb84523cb72e40875198a364cd
Niranjan Shah <niranjanshah474@gmail.com>
2023-08-16 12:55:50 +1000

    image loader added for player

--- player.py
+++ player.py
@@ -1,18 +1,36 @@
 import pygame
 from settings import *
+from support import *
+
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, group):
         super().__init__(group)
+        self.animations = None
 
-        self.image = pygame.Surface((32,64))
-        self.image.fill('green')
+        self.import_assets()
+        self.status = 'down_idle'
+        self.frame_index = 0
+
+        self.image = self.animations[self.status][self.frame_index]
         self.rect = self.image.get_rect(center=pos)
 
         # movement attributes
         self.direction = pygame.math.Vector2()
         self.pos = pygame.math.Vector2(self.rect.center)
         self.speed = 200
+
+
+    def import_assets(self):
+        self.animations = {'up': [], 'down': [], 'left': [], 'right': [],
+                           'right_idle': [], 'left_idle': [], 'up_idle': [], 'down_idle': [],
+                           'right_hoe': [], 'left_hoe': [], 'up_hoe': [], 'down_hoe': [],
+                           'right_axe': [], 'left_axe': [], 'up_axe': [], 'down_axe': [],
+                           'right_water': [], 'left_water': [], 'up_water': [], 'down_water': []}
+
+        for animation in self.animations.keys():
+            full_path = 'graphics/character/' + animation
+            self.animations[animation] = import_folder(full_path)
 
     def input(self):
         keys = pygame.key.get_pressed()
@@ -25,13 +43,13 @@ class Player(pygame.sprite.Sprite):
             self.direction.y = 0
 
         if keys[pygame.K_LEFT]:
-           self.direction.x = -1
+            self.direction.x = -1
         elif keys[pygame.K_RIGHT]:
             self.direction.x = 1
         else:
             self.direction.x = 0
 
-    def move(self,dt):
+    def move(self, dt):
 
         # normalizing a vector
         if self.direction.magnitude() > 0:
